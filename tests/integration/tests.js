89bc337b94f4002622afa48bbbe85ffe1165e098
Serenity911 <silvia.simonassi01@gmail.com>
2020-03-19 12:51:19 +0000

    updated test

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -41,6 +41,15 @@ it('should chain multiple operation and return the result of 3', function(){
 })
 
 // Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+  it('should return 3', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  })
+
   it('should return -1', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number1')).click();
@@ -49,6 +58,33 @@ it('should chain multiple operation and return the result of 3', function(){
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('-1')
   })
+
+  it('should return 1.5', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.5')
+  })
+
+  it('should return 14700', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('14900')
+  })
+  
+
+
   // What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you’d prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
   it('should return ERR if dividing 10 by 0', function(){
     running_total = element(by.css('#running_total'))
